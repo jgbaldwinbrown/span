dd8477a230f10c9a49862321f43ee6594fbd2dd7
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2025-05-30 12:09:48 -0600

    added span interval technique

--- spanint.go
+++ spanint.go
@@ -1,101 +1,139 @@
 package span
 
 import (
+	"cmp"
+	"slices"
 )
 
-type IntSet[T Spanner[int64]] struct {
-	winsize int64
-	wins map[int64][]int
-	spanners []T
-	biggestBin int
-	total int
+type OrderedSet[S Spanner[T], T cmp.Ordered] struct {
+	Center T
+	Left *OrderedSet[S, T]
+	Right *OrderedSet[S, T]
+	StartSorted []S
+	EndSorted []S
 }
 
-func (s *IntSet[T]) Spanners() []T {
-	return s.spanners
-}
-
-func leftWin(winsize, pos int64) int64 {
-	return (pos / winsize) * pos
-}
-
-func rightWin(winsize, pos int64) int64 {
-	if pos % winsize == 0 {
-		return pos
+func (s *OrderedSet[S, T]) Touching(t S) bool {
+	if s == nil {
+		return false
 	}
-	return ((pos / winsize) + 1) * pos
-}
-
-func (s *IntSet[T]) Add(t T) {
-	s.spanners = append(s.spanners, t)
-	idx := len(s.spanners) - 1
-	l, r := t.Left(), t.Right()
-	lw, rw := leftWin(s.winsize, l), rightWin(s.winsize, r)
-	for i := lw; i < rw; i += s.winsize {
-		s.wins[i] = append(s.wins[i], idx)
-		s.biggestBin = max(s.biggestBin, len(s.wins[i]))
-	}
-	s.total++
-}
-
-func (s *IntSet[T]) AddDynamic(t T) {
-	s.Add(t)
-	if s.winsize > (1 << 10) && s.biggestBin > 100 && s.biggestBin > s.total / 200 {
-		s2 := NewIntSet(s.winsize / 2, s.Spanners()...)
-		*s = *s2
-	}
-}
-
-func (s *IntSet[T]) addHitsIdxs(dst map[int]struct{}, t T) {
-	l, r := t.Left(), t.Right()
-	lw, rw := leftWin(s.winsize, l), rightWin(s.winsize, r)
-	for i := lw; i < rw; i += s.winsize {
-		for _, spidx := range s.wins[i] {
-			if Touching(t, s.spanners[spidx]) {
-				dst[spidx] = struct{}{}
-			}
-		}
-	}
-}
-
-func (s *IntSet[T]) FindHits(dst []T, t T) []T {
-	m := map[int]struct{}{}
-	s.addHitsIdxs(m, t)
-	for idx, _ := range m {
-		dst = append(dst, s.spanners[idx])
-	}
-	return dst
-}
-
-func (s *IntSet[T]) Touching(t Spanner[int64]) bool {
-	l, r := t.Left(), t.Right()
-	lw, rw := leftWin(s.winsize, l), rightWin(s.winsize, r)
-	for i := lw; i < rw; i += s.winsize {
-		for _, spidx := range s.wins[i] {
-			if Touching(t, s.spanners[spidx]) {
+	if SpanTouchingPoint(t, s.Center) {
+		for _, span := range s.StartSorted {
+			if Touching(span, t) {
 				return true
 			}
 		}
+		if s.Left.Touching(t) {
+			return true
+		}
+		return s.Right.Touching(t)
 	}
-	return false
+	if t.Right() <= s.Center {
+		for _, span := range s.StartSorted {
+			if span.Left() >= t.Right() {
+				break
+			}
+			if Touching(span, t) {
+				return true
+			}
+		}
+		return s.Left.Touching(t)
+	}
+	for i := len(s.EndSorted)-1; i >= 0; i-- {
+		span := s.EndSorted[i]
+		if span.Right() <= t.Left() {
+			break
+		}
+		if Touching(span, t) {
+			return true
+		}
+	}
+	return s.Right.Touching(t)
 }
 
-func NewIntSet[T Spanner[int64]](winsize int64, values ...T) *IntSet[T] {
-	s := &IntSet[T]{}
-	s.wins = map[int64][]int{}
-	s.winsize = winsize
-	for _, t := range values {
-		s.Add(t)
-	}
-	return s
+func SpanTouchingPoint[S Spanner[T], T cmp.Ordered](s S, t T) bool {
+	return t >= s.Left() && t < s.Right()
 }
 
-func NewIntSetDynamic[T Spanner[int64]](values ...T) *IntSet[T] {
-	s := &IntSet[T]{}
-	s.wins = map[int64][]int{}
-	s.winsize = 1 << 32
-	for _, t := range values {
-		s.AddDynamic(t)
+func (s *OrderedSet[S, T]) TouchingPoint(t T) bool {
+	if s == nil {
+		return false
 	}
-	return s
+	if t == s.Center {
+		for _, span := range s.StartSorted {
+			if SpanTouchingPoint(span, t) {
+				return true
+			}
+		}
+		return false
+	}
+	if t < s.Center {
+		for _, span := range s.StartSorted {
+			if SpanTouchingPoint(span, t) {
+				return true
+			}
+			break
+		}
+		return s.Left.TouchingPoint(t)
+	}
+	for i := len(s.EndSorted) - 1; i >= 0; i-- {
+		span := s.EndSorted[i]
+		if SpanTouchingPoint(span, t) {
+			return true
+		}
+		break
+	}
+	return s.Right.TouchingPoint(t)
+}
+
+func CmpLeft[S Spanner[T], T cmp.Ordered](a, b S) int {
+	if a.Left() < b.Left() {
+		return -1
+	}
+	if b.Left() < a.Left() {
+		return 1
+	}
+	return 0
+}
+
+func CmpRight[S Spanner[T], T cmp.Ordered](a, b S) int {
+	if a.Right() < b.Right() {
+		return -1
+	}
+	if b.Right() < a.Right() {
+		return 1
+	}
+	return 0
+}
+
+func NewOrderedSet[S Spanner[T], T cmp.Ordered](values []S) *OrderedSet[S, T] {
+	if len(values) < 1 {
+		return nil
+	}
+	if !slices.IsSortedFunc(values, CmpLeft) {
+		slices.SortFunc(values, CmpLeft)
+	}
+	toLeft := []S{}
+	toRight := []S{}
+	set := &OrderedSet[S, T]{
+		Center: values[len(values)/2].Left(),
+	}
+	for _, val := range values {
+		if set.Center >= val.Left() && set.Center < val.Right() {
+			set.StartSorted = append(set.StartSorted, val)
+		} else if set.Center >= val.Right() {
+			toLeft = append(toLeft, val)
+		} else {
+			toRight = append(toRight, val)
+		}
+	}
+	set.EndSorted = slices.Clone(set.StartSorted)
+	slices.SortFunc(set.EndSorted, CmpRight)
+	set.Left = NewOrderedSet(toLeft)
+	set.Right = NewOrderedSet(toRight)
+	return set
+}
+
+func NewOrderedSetDynamic[S Spanner[T], T cmp.Ordered](values []S) *OrderedSet[S, T] {
+	return NewOrderedSet(values)
 }
