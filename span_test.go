d46474387300cf07cda4a1c3d7ae8b70c5e59638
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2025-05-30 13:20:04 -0600

    updated span_test to check ordered set

--- span_test.go
+++ span_test.go
@@ -12,3 +12,12 @@ func TestSet(t *testing.T) {
 	s.Add(Span[int]{3, 4})
 	fmt.Println(s)
 }
+
+func TestSet2(t *testing.T) {
+	set := NewOrderedSet[Span[int64], int64]([]Span[int64]{Span[int64]{0,10}, Span[int64]{20, 30}, Span[int64]{15,16}})
+	fmt.Println(set.Touching(Span[int64]{3,5}))
+	fmt.Println(set.Touching(Span[int64]{8, 12}))
+	fmt.Println(set.Touching(Span[int64]{10,11}))
+	fmt.Println(set.Touching(Span[int64]{18, 22}))
+	fmt.Println(set.Touching(Span[int64]{18, 20}))
+}
